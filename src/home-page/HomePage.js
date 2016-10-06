6bb4358de23c19215cdee1ea3e34bceebc5427c7
Loji <loji.github@arkakodera.pl>
2016-10-06 22:44:28 +0200

    Adding animejs library;

--- src/home-page/HomePage.js
+++ src/home-page/HomePage.js
@@ -1,15 +1,51 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import './HomePage.scss';
+
+import anime from 'animejs';
+
 
 import uglyHTML from './home.js';
 
 class HomePage extends Component {
-    render() {
+    componentDidMount() {
+
+        var setDashoffset = function(el) {
+            var l = el.getTotalLength();
+            el.setAttribute('stroke-dasharray', l);
+            return [-l, 0];
+        }
+
+        var letters = anime({
+            targets: 'path.circle',
+            transform: ['translate(0 -150)', 'translate(0 0)'],
+            duration: 2400,
+            delay: 600
+        });
+
+        var lines = anime({
+            targets: ['#line-left', '#line-right'],
+            strokeDashoffset: setDashoffset,
+            duration: 2000,
+            easing: 'easeOutCubic',
+            complete: function() {
+                document.documentElement.className += ' animation-done';
+            }
+        });
+    }
+
+        render() {
         // for future use, to change components
-        return (
-            <div dangerouslySetInnerHTML={{__html: uglyHTML}} />
+        return ( <
+            div dangerouslySetInnerHTML = {
+                {
+                    __html: uglyHTML
+                }
+            }
+            />
         );
-    } 
+    }
 }
 
 export default HomePage;
