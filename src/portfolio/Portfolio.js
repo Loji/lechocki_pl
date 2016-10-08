a098deaa703bf6067678933ad28fbe9e6804c39f
Loji <loji.github@arkakodera.pl>
2016-10-08 11:07:47 +0200

    Added loading portfolio items from external resource

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -2,25 +2,48 @@ import React, {
     Component
 } from 'react';
 // import './HomePage.scss';
+import {Request} from 'request';
 
 import PortfolioItem from './PortfolioItem';
 
 class Portfolio extends Component {
 
-    // componentDidMount() {
-        
-    // }
+    constructor(state) {
+        super(state);
+
+        this.state = {
+            portfolioItems: [],
+        }
+    }
+
+    componentDidMount() {
+        let serverRequest = require('request');
+        serverRequest('https://arkakodera.pl/posts/', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body) // Show the HTML for the Google homepage.
+                this.setState({
+                    portfolioItems: JSON.parse(body),
+                })
+
+            }
+        }.bind(this))
+    }
 
     render() {
-        return ( 
+        return (
             <div>
-                <PortfolioItem 
-                    name="test"
-                    thumbnail="https://placehold.it/350x150"
-                    />
+                {this.state.portfolioItems.map((portfolioItem) => (
+                    <PortfolioItem
+                        name={portfolioItem.title}
+                        thumbnail={portfolioItem.image}
+                        />
+                )) }
+
+
             </div>
         );
     }
 }
+// hardcoded api entrypoint url
 
 export default Portfolio;
--- src/portfolio/PortfolioItem.js
+++ src/portfolio/PortfolioItem.js
@@ -11,12 +11,11 @@ class PortfolioItem extends Component {
     render() {
         return (
             <div>
-                <img src="{this.props.thumbnail}" />
+                <img src={this.props.thumbnail} />
                 <h5>{this.props.name}</h5>
-
             </div>
         );
     }
 }
-//https://arkakodera.pl/wp-json/wp/v2/posts
+
 export default PortfolioItem;
