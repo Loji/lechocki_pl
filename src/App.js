5f4a05f5f29c28c601b3a0e4fc1a01efc0a98d43
Loji <loji.github@arkakodera.pl>
2016-10-08 02:17:40 +0200

    Moving animation stuff directly to app component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,35 @@ import './App.scss';
 
 import HomePage from './home-page/HomePage.js';
 
+import anime from 'animejs';
+
 class App extends Component {
+  componentDidMount() {
+
+    var setDashoffset = function (el) {
+      var l = el.getTotalLength();
+      el.setAttribute('stroke-dasharray', l);
+      return [-l, 0];
+    }
+
+    var letters = anime({
+      targets: 'path.circle',
+      transform: ['translate(0 -150)', 'translate(0 0)'],
+      duration: 2400,
+      delay: 600
+    });
+
+    var lines = anime({
+      targets: ['#line-left', '#line-right'],
+      strokeDashoffset: setDashoffset,
+      duration: 2000,
+      easing: 'easeOutCubic',
+      complete: function () {
+        document.documentElement.className += ' animation-done';
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
